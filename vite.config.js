da14aa6cfa6f7786afe6136239b516e553143df3
serm5902 <serm5902@gmail.com>
2023-10-17 18:08:00 -0500

    Correcciones

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Serm5902.github.io/bootstrap-react-vite",
+  base: "/bootstrap-react-vite/",
 });
