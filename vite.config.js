4e5016569f7984a5ced806c62b77ba7eb546c854
serm5902 <serm5902@gmail.com>
2023-10-17 17:44:56 -0500

    Correcciones gh-pager

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bootstrap-react-vite/",
+  base: "https://Serm5902.github.io/bootstrap-react-vite",
 });
